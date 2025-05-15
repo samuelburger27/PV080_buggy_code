a705c18da8ac09fc36caf934fc71900e16bfe894
unknown <xburger2@ad.fi.muni.cz>
2025-05-15 15:03:14 +0200

    fix errors

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,13 +25,13 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    if (urllib_version == 2 or urllib_version == 3)
+    if (urllib_version == 2 or urllib_version == 3):
     	exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
     try: 
         http = urllib.PoolManager()
-    except:
+    except NameError:
         print('Exception')
 
 
