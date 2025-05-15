32b36f1394b24db0acc8ba678fcdb3f3776d0e7f
unknown <xburger2@ad.fi.muni.cz>
2025-05-15 14:46:04 +0200

    fix errors

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -30,7 +30,6 @@ def fetch_website(urllib_version, url):
  
     try: 
         http = urllib.PoolManager()
-        r = http.request('GET', url)
     except:
         print('Exception')
 
@@ -39,11 +38,13 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
